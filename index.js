e705391a72ae540b2670d9c7fb8265da756f9f48
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-01-11 09:23:41 -0500

    commit tree f00037e8588361144fc31e7d603ccc1e90c5125f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+const gifts = ["Teddy Bear", "Drone", "Doll"];
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+        return gifts;
+}  wrapGifts(gifts);
+
+
